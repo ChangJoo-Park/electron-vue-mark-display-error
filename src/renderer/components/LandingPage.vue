ea78189a170325e250f98bf5f1b94e01d8f9b067
ChangJoo Park <changjoo.park@iplateia.com>
2019-08-15 17:33:14 +0900

    Initialize Commit

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <mark-display
+    :markdown="markdown"
+    @title="setTitle"
+    keyboard-ctrl
+    url-hash-ctrl
+    auto-font-size
+    auto-blank-target
+  ></mark-display>
+</template>
+
+<script>
+import MarkDisplay from 'vue-mark-display'
+
+const markdown = `# Hello World
+----
+This is Vue Mark Display`
+
+export default {
+  components: { MarkDisplay },
+  data() {
+    return { markdown }
+  },
+  methods: {
+    setTitle({ title }) {
+      document.title = title
+    }
+  }
+}
+</script>
+
+<style>
+</style>
